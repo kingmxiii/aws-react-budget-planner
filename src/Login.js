51becad078f20105e57e276be813ff224128b04f
Rey Polanco <reypolanco@gmail.com>
2020-02-11 20:13:28 -0500

    tweak login component style

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import { Auth } from 'aws-amplify'
+import googleLogo from './Logo.svg.webp'
 
 const Login = () => (
-	<div>
-		<button onClick={() => Auth.federatedSignIn({ provider: 'Google' })}>
-			Sign in with Google
-		</button>
+	<div className="row d-flex justify-content-center align-items-center p-5">
+		<div className="p-5 border">
+			<span
+				class="btn btn-outline-dark"
+				onClick={() => Auth.federatedSignIn({ provider: 'Google' })}
+				role="button"
+				style={{ textTransform: 'none' }}>
+				<img
+					width="20px"
+					style={{ marginBottom: '3px', marginRight: '5px' }}
+					alt="Google sign-in"
+					src={googleLogo}
+				/>
+				Login with Google
+			</span>
+		</div>
 	</div>
 )
 
